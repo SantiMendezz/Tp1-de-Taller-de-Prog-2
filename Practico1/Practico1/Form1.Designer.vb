0d05aed50da06e20e3ceeb737b8cabc8033bdff9
Santiago Alfredo Méndez <93354018+SantiMendezz@users.noreply.github.com>
2022-08-24 19:22:54 -0300

    Add files via upload

---
+++ Practico1/Practico1/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BGuardar = New System.Windows.Forms.Button()
+        Me.BEliminar = New System.Windows.Forms.Button()
+        Me.LApellido = New System.Windows.Forms.Label()
+        Me.LNombre = New System.Windows.Forms.Label()
+        Me.TBApellido = New System.Windows.Forms.TextBox()
+        Me.TBNombre = New System.Windows.Forms.TextBox()
+        Me.TBCompleto = New System.Windows.Forms.TextBox()
+        Me.BSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BGuardar
+        '
+        Me.BGuardar.Location = New System.Drawing.Point(12, 134)
+        Me.BGuardar.Name = "BGuardar"
+        Me.BGuardar.Size = New System.Drawing.Size(112, 34)
+        Me.BGuardar.TabIndex = 0
+        Me.BGuardar.Text = "Guardar"
+        Me.BGuardar.UseVisualStyleBackColor = True
+        '
+        'BEliminar
+        '
+        Me.BEliminar.Location = New System.Drawing.Point(147, 134)
+        Me.BEliminar.Name = "BEliminar"
+        Me.BEliminar.Size = New System.Drawing.Size(112, 34)
+        Me.BEliminar.TabIndex = 1
+        Me.BEliminar.Text = "Eliminar"
+        Me.BEliminar.UseVisualStyleBackColor = True
+        '
+        'LApellido
+        '
+        Me.LApellido.AutoSize = True
+        Me.LApellido.Location = New System.Drawing.Point(12, 33)
+        Me.LApellido.Name = "LApellido"
+        Me.LApellido.Size = New System.Drawing.Size(78, 25)
+        Me.LApellido.TabIndex = 2
+        Me.LApellido.Text = "Apellido"
+        '
+        'LNombre
+        '
+        Me.LNombre.AutoSize = True
+        Me.LNombre.Location = New System.Drawing.Point(12, 80)
+        Me.LNombre.Name = "LNombre"
+        Me.LNombre.Size = New System.Drawing.Size(78, 25)
+        Me.LNombre.TabIndex = 3
+        Me.LNombre.Text = "Nombre"
+        '
+        'TBApellido
+        '
+        Me.TBApellido.Location = New System.Drawing.Point(109, 30)
+        Me.TBApellido.Name = "TBApellido"
+        Me.TBApellido.Size = New System.Drawing.Size(150, 31)
+        Me.TBApellido.TabIndex = 4
+        '
+        'TBNombre
+        '
+        Me.TBNombre.Location = New System.Drawing.Point(109, 77)
+        Me.TBNombre.Name = "TBNombre"
+        Me.TBNombre.Size = New System.Drawing.Size(150, 31)
+        Me.TBNombre.TabIndex = 5
+        '
+        'TBCompleto
+        '
+        Me.TBCompleto.Location = New System.Drawing.Point(306, 52)
+        Me.TBCompleto.Multiline = True
+        Me.TBCompleto.Name = "TBCompleto"
+        Me.TBCompleto.Size = New System.Drawing.Size(150, 169)
+        Me.TBCompleto.TabIndex = 6
+        '
+        'BSalir
+        '
+        Me.BSalir.Location = New System.Drawing.Point(323, 12)
+        Me.BSalir.Name = "BSalir"
+        Me.BSalir.Size = New System.Drawing.Size(112, 34)
+        Me.BSalir.TabIndex = 7
+        Me.BSalir.Text = "Salir"
+        Me.BSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(478, 244)
+        Me.Controls.Add(Me.BSalir)
+        Me.Controls.Add(Me.TBCompleto)
+        Me.Controls.Add(Me.TBNombre)
+        Me.Controls.Add(Me.TBApellido)
+        Me.Controls.Add(Me.LNombre)
+        Me.Controls.Add(Me.LApellido)
+        Me.Controls.Add(Me.BEliminar)
+        Me.Controls.Add(Me.BGuardar)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mi primer Forms"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents TBApellido As TextBox
+    Friend WithEvents TBNombre As TextBox
+    Friend WithEvents TBCompleto As TextBox
+    Friend WithEvents BSalir As Button
+End Class
---
+++ Practico1/Practico1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Dim nombre, apellido As String
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        TBCompleto.Clear()
+    End Sub
+
+    Private Sub BSalir_Click(sender As Object, e As EventArgs) Handles BSalir.Click
+        End
+    End Sub
+
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        Dim concatenar As String
+        nombre = TBNombre.Text
+        apellido = TBApellido.Text
+        concatenar = nombre & " " & apellido
+        TBCompleto.Text = concatenar
+    End Sub
+End Class
